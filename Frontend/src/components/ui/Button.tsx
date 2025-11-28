c307e27822f0b6ca052b037505f4f770e6600fc6
Simon Olsson <scmolsson@pm.me>
2025-11-28 16:02:19 +0100

    adjust button colors and shadows for better contrast

--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -9,19 +9,19 @@ type ButtonProps = {
 
 function Button({children, className, variant = "primary", ...props}: ButtonProps) {
 
-const variants: Record<ButtonVariant, string> = {
-    primary: "bg-sky-300 border-sky-900 border-sky-900 border-sky-900 hover:bg-sky-200",
-  secondary: "bg-lime-300 border-lime-900 shadow-lime-900 text-lime-900 hover:bg-lime-200",
-  link: "bg-fuchsia-200 border-fuchsia-900 shadow-fuchsia-900 text-fuchsia-900 hover:bg-fuchsia-100",
-  submit: "bg-orange-300 border-orange-900 shadow-orange-900 text-orange-900 hover:bg-orange-200",
-  outline: "bg-transparent border-stone-900 text-stone-900 hover:bg-stone-200",
+const variants = {
+    primary: "bg-blue-600 text-white hover:bg-blue-500",
+  secondary: "bg-lime-300 hover:bg-lime-200",
+  link: "bg-sky-200 hover:bg-sky-100",
+  submit: "bg-orange-300 hover:bg-orange-200",
+  outline: "bg-transparent hover:bg-stone-200",
 };
 
 const variantStyle = variants[variant]
 
   return (
     <button className={`
-        border-3 rounded-2xl px-4 py-1 font-extrabold text-xl shadow-custom cursor-pointer
+        border-3 rounded-2xl px-4 py-1 font-bold text-xl shadow-custom cursor-pointer border-stone-900 shadow-[5px_5px_0_#1c1917]
         transition-all ease-in-out duration-300 hover:shadow-none hover:translate-y-1 hover:translate-x-1 
         ${variantStyle}
         ${className}`}
