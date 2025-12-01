4872bc06acef7d1ff2aa7643c6b20f809eba6665
Simon Olsson <scmolsson@pm.me>
2025-12-01 17:30:12 +0100

    add routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "@/routes/AppRoutes"
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider  router={router}/>;
 }
 
 export default App;
