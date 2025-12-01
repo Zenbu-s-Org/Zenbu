fcb448e5ef8dd48b13f7f242f0065b8c2111ccbb
isabellarssonn <isabel.larsson@hotmail.com>
2025-12-01 11:43:02 +0100

    done with header

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,29 +1,5 @@
-import Header from "./components/Header";
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-
-const router = createBrowserRouter([
-  {
-    path : '/',
-    element: <Header />,
-  }
-]);
-
 function App() {
-  return (
-    <div className="app">
-      <RouterProvider router={router} />
-    </div>
-  )
+  return <div>App</div>;
 }
 
-export default App
-
-
-
-// function App() {
-//   return <div>
-//     <Header />
-//   </div>;
-// }
-
-// export default App;
+export default App;
