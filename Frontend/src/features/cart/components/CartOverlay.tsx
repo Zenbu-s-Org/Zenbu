4b0773cf01cde523a0f2715ae26b23fba88d1371
Simon Olsson <scmolsson@pm.me>
2025-12-01 16:12:58 +0100

    add. Cart feature with useCart hook for cart functionality

---
+++ Frontend/src/features/cart/components/CartOverlay.tsx
@@ -0,0 +1,31 @@
+import { Button, Container } from "@/components/ui"
+import Counter from "./Counter"
+import { useCart } from "../hooks/useCart"
+
+type OverlayProps = {
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+
+}
+
+function CartOverlay({setOpen}: OverlayProps) {
+const {items, getTotalPrice} = useCart()
+const totalPrice = getTotalPrice()
+
+  return (
+    <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-80 z-10">
+    <Container className="flex flex-col items-center gap-2 min-h-60">
+        <h1 className="font-bold text-2xl border-b border-b-stone-200 mb-2 w-full text-center">Your Cart</h1>
+        {items.map(item => (
+            <Counter id={item.id} key={item.id}/>
+        ))}
+     <span className="mt-2 font-bold text-2xl">{`${totalPrice} SEK`}</span>   
+    <Button variant="submit" className=" mb-2">View Order</Button>
+    <button className="border-b-2 border-stone-900 text-xl px-2 mb-2"
+    onClick={() => setOpen(false)}
+    > Close Cart </button>
+    </Container>
+    </div>
+  )
+}
+
+export default CartOverlay
---
+++ Frontend/src/features/cart/hooks/useCart.ts
@@ -0,0 +1,94 @@
+import {create} from "zustand"
+
+export type CartItem = {
+    id: string,
+    name: string,
+    price: number,
+    qty: number,
+}
+
+function loadCart(): CartItem[] {
+    const data = localStorage.getItem("cart")
+    return data ? JSON.parse(data) : []
+}
+
+function saveCart (items: CartItem[]) {
+    localStorage.setItem("cart", JSON.stringify(items))
+}
+
+type CartState = {
+    items: CartItem[];
+    addItem: (item: Omit<CartItem, "qty">) => void
+    removeItem: (id: string) => void
+    increaseQty: (id: string) => void 
+    decreaseQty: (id: string) => void
+    // clearCart: () => void
+
+    getItemQty: (id: string) => number
+    getTotalqty: () => number
+    getTotalPrice: () => number
+}
+
+export const useCart = create<CartState>((set, get) => ({
+    items: loadCart(),
+
+    addItem: (item) => {
+        const cart = get().items
+        const exists = cart.find((i) => i.id === item.id)
+
+        let updated: CartItem[];
+        if(exists) {
+            updated = cart.map((i) => i.id === item.id ? {...i, qty: i.qty + 1} : i )
+        } else {
+            updated = [...cart, {...item, qty: 1}]
+        }
+        saveCart(updated)
+        set({items: updated})
+    },
+
+    removeItem: (id) => {
+        const cart = get().items
+        const updated = cart.filter((item) => item.id !== id)
+        saveCart(updated)
+        set({items: updated})
+    },
+
+    increaseQty: (id) => {
+        const cart = get().items
+        const updated = cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item)
+        saveCart(updated)
+        set({items: updated})
+    },
+
+    decreaseQty: (id) => {
+        const cart = get().items
+        const updated = cart.map((item) => item.id === id ? {...item, qty: item.qty - 1} : item).filter((item) => item.qty > 0)
+        saveCart(updated)
+        set({items: updated})
+    },
+
+    getItemQty: (id) => {
+        const cart = get().items
+        const found = cart.find((item) => item.id === id)
+        if(found) {
+            return found.qty
+        } else {
+            return 0
+        }
+    },
+    getTotalqty: () => {
+        const cart = get().items
+        const total = cart.reduce((sum, item) => sum + item.qty, 0)
+        if(total) {
+            return total 
+        } else {
+            return 0
+        }
+    },
+    getTotalPrice: () => {
+        const cart = get().items
+        const total = cart.reduce((sum, item) => sum + item.qty * item.price, 0)
+        return total
+    }
+
+}))
