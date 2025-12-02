54d51636bcf88ceac138ec11596369b685544af1
axnerhagglund <axnerhagglund@gmail.com>
2025-12-02 13:42:04 +0100

    moved images to assets, fisnished banner

--- Frontend/src/pages/homepage/components/Banner.tsx
+++ Frontend/src/pages/homepage/components/Banner.tsx
@@ -4,7 +4,9 @@
 function Banner() {
   return (
     <main className='mt-15'>
-       <ul className='flex flex-row p-1 bg-amber-600 border-b-2 border-t-2 gap-5 rotate-6 font-medium justify-center w-full'>
+       <ul className='flex flex-row p-1 bg-amber-600 border-b-2 border-t-2 gap-5 rotate-6 font-medium justify-center w-full overflow-hidden whitespace-nowrap'>
+            <li>READY TO ORDER</li>
+            <li>MADE WITH LOVE</li>
             <li>READY TO ORDER</li>
             <li>MADE WITH LOVE</li>
        </ul>
