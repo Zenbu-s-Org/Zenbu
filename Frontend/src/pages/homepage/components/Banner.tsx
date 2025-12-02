7058fa4ee6a193c76f8a0707936df2bc08a25024
axnerhagglund <axnerhagglund@gmail.com>
2025-12-02 14:33:12 +0100

    fixed banner

--- Frontend/src/pages/homepage/components/Banner.tsx
+++ Frontend/src/pages/homepage/components/Banner.tsx
@@ -3,13 +3,17 @@
 
 function Banner() {
   return (
-    <main className='mt-15'>
-       <ul className='flex flex-row font-[Bricolage_Grotesque] p-1 bg-amber-600 border-b-2 border-t-2 gap-5 rotate-6 font-medium justify-center w-full overflow-hidden whitespace-nowrap'>
+    <main className='mt-20 mb-10'>
+      
+        <ul className='flex flex-row font-[Bricolage_Grotesque] -mx-5 p-1 bg-amber-600 border-b-2 border-t-2 gap-5 rotate-6 font-medium justify-center whitespace-nowrap overflow-hidden'>
             <li>READY TO ORDER</li>
             <li>MADE WITH LOVE</li>
             <li>READY TO ORDER</li>
             <li>MADE WITH LOVE</li>
        </ul>
+
+      
+       
     </main>
   )
 }
