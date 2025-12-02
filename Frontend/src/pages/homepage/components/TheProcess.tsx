ba9bbb934e9dffe69415a0c09ecc73856ac2bffe
axnerhagglund <axnerhagglund@gmail.com>
2025-12-02 11:10:40 +0100

    refactored code, more clean and reacty.

--- Frontend/src/pages/homepage/components/TheProcess.tsx
+++ Frontend/src/pages/homepage/components/TheProcess.tsx
@@ -5,18 +5,34 @@ import { Pagination } from 'swiper/modules';
 import "swiper/css";
 import "swiper/css/pagination";
 
-// type containerProps = {
-//     title: string,
-//     text:string
+type containerProps = {
+    variant: string,
+    title: string,
+    text: string,
+}
+
+const containerInfo: containerProps[] = [
+    {
+        variant: "sky",
+        title: "Step 1 - Order",
+        text: "Browse our menu and place your order through our app or website. Choose your favorite dishes and customize them to your liking."
     
-// }
+    },
+    {
+        variant: "green",
+        title: "Step 2 - Prepare",
+        text: "Our kitchen team prepares your meal fresh to order using premium ingredients. Every dish is carefully crafted to meet our quality standards. "
+    
+    },
+     {
+        variant: "orange",
+        title: "Step 3 - Come get it!",
+        text: "Your order is packaged for you to get it!. Enjoy restaurant-quality food in the comfort of your home."
+    
+    },
 
-// const containerInfo: containerProps[] = [
-//     {title: "Step 1 - Order" , text: "blablabla"},
-//     {title: "Step 2 - Order" , text: "blablabla"},
-//     {title: "Step 3 - Order" , text: "blablabla"},
-// ]
 
+]
 
 function TheProcess() {
 
@@ -28,41 +44,27 @@ function TheProcess() {
         <p className='mb-3'>Follow along how we do things</p>
         </section>
         <Swiper pagination={true} modules={[Pagination]} slidesPerView={1} className="mySwiper" >
-            <SwiperSlide>
+            {containerInfo.map((con,index) => (
+                <SwiperSlide key={index}>
                 <div className='p-6'>
-                <Container className='px-4' variant='sky'>
-                    <section className='flex items-center gap-5'>
+                <Container className='p-3' variant={con.variant}>
+                    <section className='flex flex-col items-center gap-5 w-[300px] h-[220px]'>
+                        <section className='flex items-center w-full px-4'>
                         <img src="/flame-outline.png" alt="icon fire emoji" />
-                        <p className='font-medium'>Step 1 - Order</p>
+                        <p className='font-medium'>{con.title}</p>
+                        </section>
+                        <p className='max-w-[250px]' >{con.text}</p>
                     </section>
-                    <p className='max-w-[250px]' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, recusandae voluptates! Quaerat,</p>
+                    
                 </Container>
                  </div>   
                 
             </SwiperSlide>
-            <SwiperSlide>
-                <div className='p-6'>
-                 <Container variant='green'>
-                    <section className='flex items-center gap-5'>
-                        <img src="/flame-outline.png" alt="icon fire emoji" />
-                        <p className='font-medium'>Step 1 - Order</p>
-                    </section>
-                    <p className='max-w-[250px]' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, recusandae voluptates! Quaerat,</p>
-                </Container>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='p-6'>
-                 <Container variant='orange'>
-                    <section className='flex items-center gap-5'>
-                        <img src="/flame-outline.png" alt="icon fire emoji" />
-                        <p className='font-medium'>Step 1 - Order</p>
-                    </section>
-                    <p className='max-w-[250px]' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, recusandae voluptates! Quaerat,</p>
-                    
-                </Container>
-                </div>
-            </SwiperSlide>
+
+            ))}
+            
+           
+            
          </Swiper>
                  
        
--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "@/layouts/MainLayout"
-
+import HomePage from "@/pages/homepage/HomePage"
 const router = createBrowserRouter([
     {
         element: <MainLayout />,
         children: [
-            {path: "/", element: <div>home</div>}
+            {path: "/", element: <div><HomePage/></div>}
         ]
     },
         {
