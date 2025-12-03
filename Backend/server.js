8dd664bf51e143aedc25a4425e0bca8590a123f0
isabellarssonn <isabel.larsson@hotmail.com>
2025-12-03 09:56:20 +0100

    fixed header links and deleted data mapp

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Starta servern + koppla DB
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
