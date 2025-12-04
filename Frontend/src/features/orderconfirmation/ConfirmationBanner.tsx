b463ffde817355b878bd5daca06007809fbf68c1
axnerhagglund <axnerhagglund@gmail.com>
2025-12-04 08:45:13 +0100

    confirmationbanner ready for pr

--- Frontend/src/features/orderconfirmation/ConfirmationBanner.tsx
+++ Frontend/src/features/orderconfirmation/ConfirmationBanner.tsx
@@ -2,48 +2,52 @@
 import { cn } from 'clsx-for-tailwind'
 import { ChevronDown } from 'lucide-react'
 import { ChevronUp } from 'lucide-react'
-import { useState,useEffect, use } from 'react'
+import { useState,useEffect,} from 'react'
 import Order from './components/Order'
-
+type itemprops = {
+        menuItem: string,
+        quantity: number
+    }
 function ConfirmationBanner() {
     const [toggle, setToggle] = useState<boolean>(false)
-    const [qty, setQty] = useState<string>("")
-    const [item, setItem] = useState<string>("")
+    const [items, setItems] = useState<itemprops[]>([])
     const [status, setStatus] = useState<string>("")
     const [orderId, setOrderId] = useState<string | null>()
-   
+
     useEffect(() => {
         const id = localStorage.getItem("orderId")
         setOrderId(id)
-        
     },[])
 
     useEffect(() => {
         async function fetchData(){
-        const res = await fetch(`http://localhost:5000/api/orders`)
-        const orders = await res.json()
-        setQty(orders[0].items[0].quantity)
-        setStatus(orders[0].status)
-        setItem("Spicy Bowl")
-        
+        if (!orderId) return
+        try {
+            const res = await fetch(`http://localhost:5000/api/orders/${orderId}`)
+            if(!res.ok){
+                console.log("order not found")
+                return;
+            }
+            
+
+            const order = await res.json()
+            console.log(order)
+            console.log("ORDER:", order)
+            console.log("ITEMS:", order.items)
+            setItems(order.items)
+            setStatus(order.status)
+        } catch (error) {
+            console.error("error", error)
+        }
     }
     fetchData()
-    },[])
+    },[orderId])
 
-
-    //mockDATA 
-    type mockprops = {
-        item: string,
-        qty: number
-    }
-    const ordereditems:mockprops[] = [
-        {item: "Sweet bowl", qty: 2},
-        {item: "Spicy bowl", qty: 1},
-        {item: "Vegan bowl", qty: 4},
-    ]
+ 
+    
 
   return (
-    <main className={cn("w-[300px] text-[#7C2D12]",
+    <main className={cn("w-[300px] text-[#7C2D12] absolute top-15 z-10",
         {"bg-orange-300": status === "pending"},
         {"bg-fuchsia-300": status === "preparing",},
         {"bg-lime-300": status === "ready",}
@@ -57,8 +61,8 @@ function ConfirmationBanner() {
         {/* {!orderId && <p className='text-center'>There is no order</p>}  */}
 
         {toggle && 
-           ordereditems.map((e,i) => (
-                    <Order key={i} title={e.item} qty={e.qty}/>
+           items.map((e,i) => (
+                    <Order key={i} title={e.menuItem} qty={e.quantity}/>
             ))
         }
         
