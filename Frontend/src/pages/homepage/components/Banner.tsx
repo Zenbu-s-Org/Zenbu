c4b19e5303c542998dae7a49500eb82f4a821ac4
axnerhagglund <axnerhagglund@gmail.com>
2025-12-04 14:08:16 +0100

    kinda fixed banner for demo

--- Frontend/src/pages/homepage/components/Banner.tsx
+++ Frontend/src/pages/homepage/components/Banner.tsx
@@ -3,18 +3,16 @@
 
 function Banner() {
   return (
-    <main className='mt-20 mb-10'>
-      
-        <ul className='flex flex-row font-[Bricolage_Grotesque] -mx-5 p-1 bg-amber-600 border-b-2 border-t-2 gap-5 rotate-6 font-medium justify-center whitespace-nowrap overflow-hidden'>
+    <section className='mt-20 mb-10'>
+        
+        <ul className='rotate-6 flex flex-row font-[Bricolage_Grotesque] p-1 bg-amber-600 border-b-2 border-t-2 gap-3 font-medium justify-center whitespace-nowrap overflow-hidden'>
             <li>READY TO ORDER</li>
             <li>MADE WITH LOVE</li>
             <li>READY TO ORDER</li>
             <li>MADE WITH LOVE</li>
        </ul>
-
-      
-       
-    </main>
+    
+    </section>
   )
 }
 
