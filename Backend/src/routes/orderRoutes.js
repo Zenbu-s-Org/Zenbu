8292abd6a9ab49c8c8a450fe0d4d77d18d868a95
axnerhagglund <axnerhagglund@gmail.com>
2025-12-08 14:43:01 +0100

    put order for status update done, fixed id with nano for more koncise and prettier

--- Backend/src/routes/orderRoutes.js
+++ Backend/src/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Order from "../models/Order.js";
 import { validateOrder } from "../middlewares/validateOrder.js";
-
+import { nanoid } from "nanoid";
 const router = express.Router();
 
 // GET-anrop för att hämta alla orders /api/orders
@@ -33,7 +33,7 @@ router.post("/", validateOrder, async (req, res) => {
     const order = await Order.create({
       ...req.body,
       status: "pending",
-      orderNumber: crypto.randomUUID(),
+      orderNumber: nanoid(7)
     });
 
     res.status(201).json({
@@ -44,5 +44,29 @@ router.post("/", validateOrder, async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+//PUT-anrop för att updatera en order
+
+router.put("/:id", validateOrder, async (req,res) => {
+  
+  const {status} = req.body
+  const updatedOrder = await Order.findOneAndUpdate(
+    {orderNumber: req.params.orderNumber}, //söker id
+    {status}, //req in body
+    {new: true}
+  )
+  res.status(200).json({
+    success: true,
+    message: "order updated",
+    order: updatedOrder
+  })
+  if(!updatedOrder){
+    res.status(404).json({
+      success: false,
+      message: "order not found"
+    })
+  }
+
+} )
 
 export default router;
