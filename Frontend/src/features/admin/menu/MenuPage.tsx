d5425fa972cbb1fbe17e520bfa387228d0800feb
Simon Olsson <scmolsson@pm.me>
2025-12-10 12:37:46 +0100

    add base logic for MenuModal to change a existing MenuItem

--- Frontend/src/features/admin/menu/MenuPage.tsx
+++ Frontend/src/features/admin/menu/MenuPage.tsx
@@ -1,18 +1,22 @@
 import MenuTable from "./components/MenuTable"
 import { useFetch } from "@/hooks/useFetch"
 import { type MenuItem } from "@/features/menu";
+import { useModal } from "@/components/modal";
+import MenuModal from "./components/MenuModal";
 
 
 function MenuPage() {
   const {data} = useFetch<MenuItem[]>("/menu") 
+  const {openModal} = useModal()
 
   if(!data) return null
+
 
   return (
     <div className="flex flex-col items-center w-full px-3 gap-3">
       <h1>Menu</h1>
       <section className="w-full px-3">
-      <MenuTable data={data} onSelect={(item) => console.log(item) }/>
+      <MenuTable data={data} onSelect={(item) =>  openModal(<MenuModal data={item}/>)}/>
 
       </section>
    </div>
--- Frontend/src/features/admin/menu/components/MenuModal.tsx
+++ Frontend/src/features/admin/menu/components/MenuModal.tsx
@@ -1,10 +1,70 @@
-import { Modal } from "@/components/modal"
+import type { MenuItem } from "@/features/menu"
+import { Button, Input } from "@/components/ui"
+import { SectionLabel } from "../../components"
+import { useState } from "react"
 
-function MenuModal() {
+type Props = {
+  data: MenuItem
+}
+
+const CATEGORY_OPTIONS = ["drink", "bowl", "extra"] as const
+
+function MenuModal({data}: Props) {
+  const [name, setName] = useState(data.name)
+  const [desc, setDesc] = useState(data.description ?? "")
+  const [price, setPrice] = useState(data.price)
+  const [category, setCategory] = useState<MenuItem["category"]>(data.category)
+
+  const handleSave = () => {
+    const updatedItem: MenuItem = {
+      ...data,
+      name,
+      description: desc,
+      price,
+      category,
+    }
+    console.log(updatedItem)
+  }
+
   return (
-    <Modal>
-        
-    </Modal>
+    <div className="flex flex-col items-center w-full">
+      <h2>{data._id}</h2>
+      <Input 
+        label="Name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Product Name"
+      />
+      <Input 
+        label="Price"
+        type="number"
+        value={price}
+        onChange={(e) => setPrice(Number(e.target.value))}
+        placeholder="Product Price"
+      />
+      <Input 
+        label="Description"
+        type="text"
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+        placeholder="Product Description"
+      />
+
+            <SectionLabel label="category">
+                  <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value as MenuItem["category"])}
+          className="border-3 border-stone-900 shadow-[5px_5px_0_#1c1917] rounded-xl px-3 py-1 w-full"
+        >
+          {CATEGORY_OPTIONS.map((option) => (
+            <option key={option} value={option}>
+              {option.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </SectionLabel>
+    </div>
   )
 }
 
