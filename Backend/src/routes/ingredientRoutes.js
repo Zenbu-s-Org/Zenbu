a2b2d11328e1a9faff40ce61fd67c25edfbd01af
axnerhagglund <axnerhagglund@gmail.com>
2025-12-11 13:34:41 +0100

    post ingredients done

--- Backend/src/routes/ingredientRoutes.js
+++ Backend/src/routes/ingredientRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Ingredient from "../models/Ingredient.js";
+import { nanoid } from "nanoid";
+import { authorize } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -13,4 +15,30 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+router.post("/",authorize, async (req,res) => {
+  try {
+    
+    const {name, category, qty} = req.body
+    const id = `ing-${nanoid(6)}`
+    const addIngredient = await Ingredient.create(
+      {
+        id: id,
+        name: name,
+        category: category,
+        qty: qty,
+        createdAt: new Date().toISOString()
+      }
+    )
+  
+    res.status(200).json({
+      success: true,
+      message: "ingredient added",
+      ingredient: addIngredient
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    
+  }
+})
 export default router;
