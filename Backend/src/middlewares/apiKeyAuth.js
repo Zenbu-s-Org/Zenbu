99ac635039ddb7d658ca35776517fbf7ac2db470
axnerhagglund <axnerhagglund@gmail.com>
2025-12-15 13:33:00 +0100

    changed apikeyMiddlware

--- Backend/src/middlewares/apiKeyAuth.js
+++ Backend/src/middlewares/apiKeyAuth.js
@@ -2,15 +2,20 @@
 
 
 export function apiKeyAuth(req,res,next){
-    const ApiKeyHeader = req.header("x-api-key")
+    const clientkey = req.get("x-api-key")
     const apikey = process.env.API_KEY
     //om nyckeln saknas i .env då får vi ett serferkonig fel.
     if(!apikey){
         //error
         return res.status(500).json({message: "server error"})
     }
-    if(!ApiKeyHeader || ApiKeyHeader !== apikey){
-        return res.status(400).json({message: "Wrong Api key"})
+    if(!clientkey){
+        return res.status(401).json({message: "Api-key required"})
+    }
+    if(!res.ok){
+        return res.status(401).json({ message: "Invalid API key" });
     }
     next()
+
+    
 }
